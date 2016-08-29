9a38513c4d24e8c9d5ff1d6182945edd8dd7e705
Belde, Vikram <belvikram@gmail.com>
2016-08-29 14:52:03 -0400

    cover flow blueprint

--- MyTest.js
+++ MyTest.js
@@ -23,6 +23,7 @@ var MyTest = React.createClass({
 // now you can handle the style object of the component with out re-rendering the component by passing the style object to the
 //setNativeProps(styleObject) i.e setNativeProps({style:{height: 100, width:100}}) .. hooorayyy
    ViewRef : {},
+   ViewRef2 : {},
    moveLR : 0,
    moveTB : 0,
    x:0,
@@ -102,16 +103,24 @@ var MyTest = React.createClass({
         //console.log("X",this.x)
         //console.log("gestureState.dx + 'deg'",gestureState.dx + 'deg');
         //console.log(typeof(gestureState.dx));
-        if(gestureState.dx < 82 && gestureState.dx > -82  ){
+        if(1 ){
           this.x = (this.x + gestureState.dx)
         } else{
           this.x =  70
 
         }
-        var Contruct = this.x + 'deg'
+        var Contruct = this.x/50 + 'deg'
     this.ViewRef.setNativeProps({
       style: {
-        transform: [{ rotateY: Contruct }]
+        //top:this.x,
+        right: this.x/100,
+        transform: [{perspective: 250},{ rotateY: Contruct }]
+      }
+    })
+
+    this.ViewRef2.setNativeProps({
+      style: {
+        transform: [{perspective: 250},{ rotateY: Contruct }]
       }
     })
   },
@@ -146,9 +155,12 @@ var MyTest = React.createClass({
 
     return (
     <View style={styles.container} {...this._panResponder.panHandlers}>
-      <Image source={{uri: 'https://facebook.github.io/react/img/logo_og.png'}}
+      <Image source={{uri: 'https://www.novoda.com/blog/content/images/2016/06/reactive-nativingitup-png-800x600_q96.png'}} resizeMode= 'contain'
       ref = {(viewReference)=>{if(viewReference != null){this.ViewRef = viewReference} }}
-          style={{width: 400, height: 400,transform: [{perspective: 500},{ rotateY: '30deg'}]}} />
+          style={{width: 200, height: 200,transform: [{perspective: 500},{ rotateY: '30deg'}]}} />
+          <Image source={{uri: 'https://www.novoda.com/blog/content/images/2016/06/reactive-nativingitup-png-800x600_q96.png'}} resizeMode= 'contain'
+          ref = {(viewReference2)=>{if(viewReference2 != null){this.ViewRef2 = viewReference2} }}
+              style={{width: 200, height: 200,transform: [{perspective: 500},{ rotateY: '30deg'}]}} />
         {/*<TouchableHighlight onPress= {()=>{this.setStyle()}}>
           <View style={{backgroundColor:'lightblue',height:30, width:100}} >
             <Text style={{backgroundColor:'lightblue',height:30, width:100}}> Click to Change Color</Text>
@@ -162,6 +174,7 @@ var MyTest = React.createClass({
 var styles = StyleSheet.create({
   container:{
     flex: 1,
+    flexDirection:'row',
     //backgroundColor:'coral',
     alignItems:'center',
     justifyContent:'center',
